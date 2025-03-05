9b9bdce753c30364adc414795a7e20d82ea7d7fd
Ventsislav Patsev <164578745+vpatsev@users.noreply.github.com>
2025-03-05 11:24:04 +0200

    Add files via upload

---
+++ 04.TextFilter/Program.cs
@@ -0,0 +1,13 @@
+﻿List<string> bannedWords = Console.ReadLine()
+                            .Split(", ")
+                            .ToList();
+
+string text = Console.ReadLine();
+
+foreach(string replaceWord in bannedWords)
+{
+    string asterix = new string('*',replaceWord.Length);
+
+   text = text.Replace(replaceWord,asterix);
+}
+Console.WriteLine(text);
---
+++ 05.DigitsLettersAndOther/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+string input = Console.ReadLine();
+
+StringBuilder digits = new StringBuilder();
+StringBuilder letters =  new StringBuilder();
+StringBuilder characters = new StringBuilder();
+
+foreach (char ch in input)
+{
+    if (char.IsDigit(ch))
+    {
+        digits.Append(ch);
+    }
+    else if (char.IsLetter(ch)) 
+    { 
+        letters.Append(ch);
+    }  
+    else
+    {
+        characters.Append(ch);
+    }
+}
+Console.WriteLine(digits);
+Console.WriteLine(letters);
+Console.WriteLine(characters);
